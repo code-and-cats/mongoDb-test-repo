d93aad55bf9e16f288d9ff56f8abdd4263b2ae1a
Vio Szabo <vio@vioszabo.se>
2023-05-08 23:28:04 +0200

    playing around a bit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from 'mongoose'
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -11,10 +12,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+if (mongoose.connection.readyState === 1) {
+  next() 
+} else {
+  res.status(503).json({error: 'service unavailable'})
+}
+})
+
+// const mongoUrl = process.env.MONGO_URL || mongoose.connect("mongodb://127.0.0.1:27017");
+mongoose.connect("mongodb://127.0.0.1", { useNewUrlParser : true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Animal = mongoose.model('Animal', {
+  name: String,
+  age: Number,
+  isFurry: Boolean
+})
+
+Animal.deleteMany().then(() => {
+new Animal({ name: 'Bullen', age: 4, isFurry: true }).save()
+new Animal({ name: 'Popcorn', age: 3, isFurry: true }).save()
+new Animal({ name: 'Seal', age: 2, isFurry: true }).save()
+})
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  Animal.find().then(animals => {
+    res.json(animals)
+  })
 });
+
+app.get('/:name', async (req, res) => {
+try {Animal.findOne({name: req.params.name}).then((animal) => {
+    if(animal) {
+      res.json(animal)
+    } else {
+      res.status(404).json({ error: 'Not found!' })
+    }
+  })
+} catch (err) {
+  res.status(400).json({error: 'Invalid request!'})
+}
+})
 
 // Start the server
 app.listen(port, () => {
